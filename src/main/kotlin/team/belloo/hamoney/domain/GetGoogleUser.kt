e43d52d26926a11254e723fd3ec4cdaea2791248
stay7 <codelatte20@gmail.com>
2023-04-05 01:16:22 +0900

    redirect 앱스킴

---
+++ src/main/kotlin/team/belloo/hamoney/domain/GetGoogleUser.kt
@@ -0,0 +1,42 @@
+package team.belloo.hamoney.domain
+
+import team.belloo.hamoney.UseCase
+import team.belloo.hamoney.client.GoogleTokenClient
+import team.belloo.hamoney.client.GoogleUserClient
+
+@UseCase
+class GetGoogleUser(
+    private val googleUserClient: GoogleUserClient,
+    private val googleTokenClient: GoogleTokenClient
+) {
+    operator fun invoke(code: String, state: String): GoogleUser {
+        val tokenResponse =
+            googleTokenClient.token(code = code, redirectUri = "http://10.0.2.2:8080/oauth/google/code")
+                .execute().let { response ->
+                    if (!response.isSuccessful) {
+                        throw IllegalStateException(response.errorBody()?.string())
+                    }
+                    response.body()!!
+                }
+
+        val googleResult =
+            googleUserClient.me(accessToken = "Bearer ${tokenResponse.access_token}").execute().let { response ->
+                if (!response.isSuccessful) {
+                    throw IllegalStateException(response.errorBody()?.string())
+                }
+                response.body()!!
+            }
+
+        return GoogleUser(
+            email = googleResult.email,
+            id = googleResult.sub,
+            verifiedEmail = googleResult.verified_email
+        )
+    }
+
+    data class GoogleUser(
+        val id: String,
+        val email: String,
+        val verifiedEmail: Boolean
+    )
+}
--- src/main/kotlin/team/belloo/hamoney/webapi/SocialSignupController.kt
+++ src/main/kotlin/team/belloo/hamoney/webapi/SocialSignupController.kt
@@ -1,30 +1,36 @@
 package team.belloo.hamoney.webapi
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.ModelAndView
+import team.belloo.hamoney.domain.GetGoogleUser
 import team.belloo.hamoney.domain.GetKakaoUser
 
-@RestController
+@Controller
 @RequestMapping("/oauth")
 class SocialSignupController(
-    private val getKakaoUser: GetKakaoUser
+    private val getKakaoUser: GetKakaoUser,
+    private val getGoogleUser: GetGoogleUser
 ) {
 
     @GetMapping("/kakao/code")
     fun kakao(
         @RequestParam code: String,
         @RequestParam state: String,
-    ) {
+    ): String {
         getKakaoUser(code, state).also { println(it) }
+        return "redirect:hamoney://"
     }
 
     @GetMapping("/google/code")
     fun google(
         @RequestParam code: String,
         @RequestParam state: String,
-    ) {
-        println("$code, $state")
+    ): String {
+        getGoogleUser(code, state).also { println(it) }
+        return "redirect:hamoney://"
     }
 }
