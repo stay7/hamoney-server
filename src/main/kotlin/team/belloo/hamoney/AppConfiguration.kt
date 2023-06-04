a1e8b8cac837e749007fd97d001c4b4f1fa8642f
stay7 <codelatte20@gmail.com>
2023-06-04 17:08:26 +0900

    baseUrl은 주입받아 쓴다

--- src/main/kotlin/team/belloo/hamoney/AppConfiguration.kt
+++ src/main/kotlin/team/belloo/hamoney/AppConfiguration.kt
@@ -27,5 +27,5 @@ class AppConfiguration(
     // flutter android는 localhost를 알 수 없기에 10.0.2.2로 호출한다
     @Bean
     fun getKakaoUser(kakaoTokenClient: KakaoTokenClient, kakaoUserClient: KakaoUserClient) =
-        GetKakaoUser(kakaoTokenClient, kakaoUserClient, "http://10.0.2.2:8080")
+        GetKakaoUser(kakaoTokenClient, kakaoUserClient, baseUrl)
 }
